f6e776e813762c475b96d0f9e8ef7e76fd92270b
AissAiss <lucasaissaoui201@gmail.com>
2022-02-04 21:22:11 +0100

    Téléchargement du fichier est plus propre et le deuxième fragement est affiché uniquement quand le fhcier est recus

--- app/src/main/java/com/example/um_planner_java/FirstFragment.java
+++ app/src/main/java/com/example/um_planner_java/FirstFragment.java
@@ -2,7 +2,10 @@ package com.example.um_planner_java;
 
 
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.URLUtil;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -23,6 +27,23 @@ import com.example.um_planner_java.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    // Identifiant du téléchargement
+    private long downloadID;
+
+    // Receveur qui écoute quand le fichier est recus.
+    private BroadcastReceiver onDownloadComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Fetching the download id received with the broadcast
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            //Checking if the received broadcast is for our enqueued download by matching download id
+            if (downloadID == id) {
+                //Toast.makeText(getContext(), "Download Completed", Toast.LENGTH_SHORT).show();
+                NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_SecondFragment);
+
+            }
+        }
+    };
 
     @Override
     public View onCreateView(
@@ -31,9 +52,12 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        requireActivity().registerReceiver(onDownloadComplete,new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
         return binding.getRoot();
 
     }
+
+
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -43,6 +67,7 @@ public class FirstFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 String getUrl = "https://proseconsult.umontpellier.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=58c99062bab31d256bee14356aca3f2423c0f022cb9660eba051b2653be722c4a5f10b982f9b914f8b3df9a16d82f493dc5c094f7d1a811b903031bde802c7f56c5ce5d7b8d9b880fb6990772f87c6e42988e4003796ffd7b370c710463ddfae935a1cf1cb9242dad5cce9713b0bbcb9352abd8b23259769b84175e9ee785c7b,1";
+                //String getUrl = "https://geo.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FGEO.2Fvar.2Fgeo.2Fstorage.2Fimages.2Fmedia.2Fsa-domestication-n-a-pas-emousse-l-instinct-de-ce-chasseur-hors-pair.2F2052028-1-fre-FR.2Fsa-domestication-n-a-pas-emousse-l-instinct-de-ce-chasseur-hors-pair.2Ejpg/1150x647/background-color/ffffff/quality/70/chat-quand-minou-devient-un-tueur-en-serie.jpg";
 
                 String title = URLUtil.guessFileName(getUrl, null, null);
 
@@ -52,13 +77,13 @@ public class FirstFragment extends Fragment {
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
                 request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, title);
 
-                DownloadManager dm = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-                dm.enqueue(request);
-
-                NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_SecondFragment);
+                DownloadManager downloadManager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+                downloadID = downloadManager.enqueue(request);
             }
         });
     }
+
+
 
     @Override
     public void onDestroyView() {
